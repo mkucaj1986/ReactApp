c4e27c91a5deaaa68415db1c004243828d8bd7a1
Cajek <mkucaj86@gmail.com>
2017-09-06 23:57:49 +0200

    add spinner

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import CartItem from "../components/CartItem";
 
 import { getData } from "../actions/getData";
+import "../style/index.scss";
 
 class App extends React.Component {
 	constructor(props) {
@@ -18,17 +19,40 @@ class App extends React.Component {
 	}
 	render() {
 		let dataDom = null;
+		let noItems = null;
 		if (this.isLoading) {
-			dataDom = <div>Loading...</div>;
+			dataDom = (
+				<div className="loading-spinner">
+					Loading...
+					<div className="sk-fading-circle">
+						<div className="sk-circle1 sk-circle" />
+						<div className="sk-circle2 sk-circle" />
+						<div className="sk-circle3 sk-circle" />
+						<div className="sk-circle4 sk-circle" />
+						<div className="sk-circle5 sk-circle" />
+						<div className="sk-circle6 sk-circle" />
+						<div className="sk-circle7 sk-circle" />
+						<div className="sk-circle8 sk-circle" />
+						<div className="sk-circle9 sk-circle" />
+						<div className="sk-circle10 sk-circle" />
+						<div className="sk-circle11 sk-circle" />
+						<div className="sk-circle12 sk-circle" />
+					</div>
+				</div>
+			);
 		}
 		if (this.props.dataLoaded) {
 			dataDom = <CartItem items={this.props.items} />;
 			this.isLoading = false;
 		}
+		if (this.props.items.length === 0 && this.props.dataLoaded) {
+			noItems = <div className="no-items">No items in Cart</div>;
+		}
 		return (
 			<div className="container">
 				<h1>7 Ninja React App</h1>
 				{dataDom}
+				{noItems}
 			</div>
 		);
 	}
