0014ba79948b5476013d759f905f20a5c96ee45a
Cajek <mkucaj86@gmail.com>
2017-09-07 12:27:17 +0200

    changes for production

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,20 +1,20 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  devtool: 'source-map',
-  entry: './src/index',
+  devtool: "source-map",
+  entry: "./src/index",
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.[hash].js',
-    publicPath: '/'
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.[hash].js",
+    publicPath: "/"
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
@@ -25,11 +25,11 @@ module.exports = {
       }
     }),
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html'
+      template: "./src/index.html",
+      filename: "index.html"
     }),
     new ExtractTextPlugin({
-      filename: 'index.[hash].css',
+      filename: "index.[hash].css",
       allChunks: true
     })
   ],
@@ -37,16 +37,27 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        use: ['babel-loader'],
-        exclude: /node_modules/,
+        use: ["babel-loader"],
+        exclude: /node_modules/
       },
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass-loader'
-        })
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: "file-loader"
       }
     ]
-  },
+  }
 };
